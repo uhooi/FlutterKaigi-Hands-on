ef1a0deaed263d68b9389f08fabd077c5250e817
uhooi <the_uhooi@yahoo.co.jp>
2021-11-30 20:16:57 +0900

    Add side

--- lib/components/core/index_template.dart
+++ lib/components/core/index_template.dart
@@ -1,6 +1,7 @@
 import '../../components/core/organisms/article_body.dart';
 import '../../components/core/organisms/footer.dart';
 import '../../components/core/organisms/image_header.dart';
+
 //import '../../components/core/organisms/side_info_body.dart';
 import 'package:flutter/material.dart';
 
@@ -25,24 +26,41 @@ class _IndexTemplateState extends State<IndexTemplate> {
           children: <Widget>[
             ImageHeaderOrganisms(data: widget.data!['header']),
             Container(
-              padding: const EdgeInsets.only(
-                top: 30,
-                bottom: 30,
-                right: 50,
-                left: 50,
-              ),
-              child: Container(
-                color: Colors.black12,
-                width: 1000,
                 padding: const EdgeInsets.only(
-                  top: 50,
-                  bottom: 50,
+                  top: 30,
+                  bottom: 30,
                   right: 50,
                   left: 50,
                 ),
-                child: ArticleBodyOrganisms(data: widget.data!['body']['articles']),
-              ),
-            ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      color: Colors.black12,
+                      width: 1000,
+                      padding: const EdgeInsets.only(
+                        top: 50,
+                        bottom: 50,
+                        right: 50,
+                        left: 50,
+                      ),
+                      child: ArticleBodyOrganisms(
+                          data: widget.data!['body']['articles']),
+                    ),
+                    Container(
+                      width: 200,
+                      padding: const EdgeInsets.only(
+                        top: 0,
+                        bottom: 0,
+                        right: 0,
+                        left: 30,
+                      ),
+                      color: Colors.white,
+                      height: 1000,
+                    ),
+                  ],
+                )),
             Container(
               padding: const EdgeInsets.only(
                 top: 0,
