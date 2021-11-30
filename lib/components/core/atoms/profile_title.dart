92d0cd60fad9f0bae5c7507307ffa12b2f1aeb6a
uhooi <the_uhooi@yahoo.co.jp>
2021-11-30 19:17:03 +0900

    Add components

---
+++ lib/components/core/atoms/profile_title.dart
@@ -0,0 +1,14 @@
+import '../../../components/common/atoms/side_info_title.dart';
+import 'package:flutter/material.dart';
+
+class ProfileTitle extends StatelessWidget {
+  const ProfileTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+      height: 10,
+    );
+  }
+}
---
+++ lib/components/core/organisms/image_header.dart
@@ -0,0 +1,12 @@
+import '../../../components/core/atoms/header_image.dart';
+import 'package:flutter/material.dart';
+
+class ImageHeaderOrganisms extends StatelessWidget {
+  const ImageHeaderOrganisms({Key? key, this.data}) : super(key: key);
+  final Map? data;
+
+  @override
+  Widget build(BuildContext context) {
+    return HeaderImage(imagePath: data!['imagePath']);
+  }
+}
