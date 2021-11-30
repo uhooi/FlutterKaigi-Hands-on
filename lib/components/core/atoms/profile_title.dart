0524666d68d93ddca4ebcb05e498c2c3ff4c7d64
uhooi <the_uhooi@yahoo.co.jp>
2021-11-30 20:30:13 +0900

    Update profile title

--- lib/components/core/atoms/profile_title.dart
+++ lib/components/core/atoms/profile_title.dart
@@ -6,9 +6,9 @@ class ProfileTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
-      height: 10,
+    return Text(
+      'プロフィール',
+      style: Theme.of(context).textTheme.subtitle1,
     );
   }
 }
